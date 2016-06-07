aa63353127dcf6cadda5b7b5b358a1742cef59e1
Bharath Bhat <bharathb777@gmail.com>
2016-06-08 02:40:36 +0800

    Removed orientation value logging on client

--- assets/SpecialSupplierTexture.js
+++ assets/SpecialSupplierTexture.js
@@ -7,7 +7,6 @@ var image = {
         var orientation = entity._rotate.z;
         ctx.strokeStyle = 'rgba(255, 0, 0, 0.5)';
         ctx.lineWidth = 5;
-        console.log(Math.degrees(orientation));
         ctx.beginPath();
         ctx.arc(0, 0, w / 2,
             3 * Math.PI / 4,

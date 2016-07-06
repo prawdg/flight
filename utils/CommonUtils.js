0383a3ed91089a73dc3ea34de0c61e20a8adf5f9
Bharath Bhat <bharathb777@gmail.com>
2016-07-06 19:39:21 +0800

    Fix refs #2 holding down fire input blocks other players fire input

--- utils/CommonUtils.js
+++ utils/CommonUtils.js
@@ -10,11 +10,9 @@ var CommonUtils = {
      */
     debounce: function (fn, threshold, scope) {
         threshold || (threshold = 250);
-        var last,
-            deferTimer;
+        var last;
         return function () {
             var context = scope || this;
-
             var now = +new Date(),
                 args = arguments;
             if (!last || now > last + threshold) {
@@ -24,6 +22,30 @@ var CommonUtils = {
             }
             return false;
         };
+    },
+
+    /**
+     * Calls the function fn with provided scope if it was not called in the last threshold ms.
+     * Note: adds a variable __debounceLast__ to the scope for bookkeeping.
+     *
+     * @param fn
+     * @param threshold
+     * @param scope
+     * @return true if the function was called, else false
+     */
+    debouncedCall: function (fn, threshold, scope) {
+        threshold || (threshold = 250);
+        var context = scope || this;
+        context.__debounceLast__ = context.__debounceLast__ || [];
+        var now = +new Date(),
+            args = arguments;
+
+        if (!context.__debounceLast__[fn] || now > context.__debounceLast__[fn] + threshold) {
+            context.__debounceLast__[fn] = now;
+            fn.apply(context, args);
+            return true;
+        }
+        return false;
     },
 
     randomInteger: function (limit) {
